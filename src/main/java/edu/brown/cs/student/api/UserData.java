a923c1c9a3b6d71d0a2e051aed0e96207cfefbe5
GoldenSlayer <codysims190@gmail.com>
2021-09-28 19:31:55 -0400

    Styling
    fixed styling

--- src/main/java/edu/brown/cs/student/api/UserData.java
+++ src/main/java/edu/brown/cs/student/api/UserData.java
@@ -7,34 +7,34 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * This class is used for collecting user data and combining users
+ * This class is used for collecting user data and combining users.
  */
 public class UserData {
   // These are used for concatenating URIs together for get requests
-  String URIP1; //part 1
-  String URIP2; //part 2
-  List<String> nums;
+  private final String url1; //part 1
+  private final String url2; //part 2
+  private final List<String> nums;
+  private final Set<User> userSet = new HashSet<>();
 
-  Set<User> userSet = new HashSet<>();
-  public UserData(){
-    nums = Arrays.asList("one","two", "three", "four", "five");
-    URIP1 = "https://runwayapi.herokuapp.com/users-";
-    URIP2 = "?auth=csims&key=" + ClientAuth.getApiKey();
+  public UserData() {
+    nums = Arrays.asList("one", "two", "three", "four", "five");
+    url1 = "https://runwayapi.herokuapp.com/users-";
+    url2 = "?auth=csims&key=" + ClientAuth.getApiKey();
   }
 
   /**
-   * Stores users in an array
+   * Stores users in an array.
    */
-  public void getData(){
+  public void getData() {
     ApiClient client = new ApiClient();
 
-
     // Cycles through all URIs so we can build a list of Users
-    for(String num: nums){
+    for (String num: nums) {
       User[] users;
       GsonParser gsonParser = new GsonParser();
-      users = gsonParser.parseUser(client.makeRequest(ClientRequestGenerator.getRequest(URIP1 + num +URIP2)));
-      if(users != null){
+      users = gsonParser.parseUser(client.makeRequest(ClientRequestGenerator.getRequest(
+          url1 + num + url2)));
+      if (users != null) {
         Collections.addAll(userSet, users);
       }
     }
@@ -43,16 +43,17 @@ public class UserData {
     userSet.remove(nullUser);
 
     //Prints each user's data
-    for (User user: userSet){
+    for (User user: userSet) {
       System.out.println(user.toString());
     }
     System.out.println(userSet.size());
   }
 
   /**
-   * Returns a list of all users
+   * Returns a list of all users.
+   * @return a set of Users
    */
-  public Set<User> getUsers(){
+  public Set<User> getUsers() {
     return userSet;
   }
 }
