1189ae03adeeb7b66d92fa43d90f4a305bb74012
dtsryBKJnugY3v2v <90650613+dtsryBKJnugY3v2v@users.noreply.github.com>
2021-10-03 18:09:12 -0400

    tiny test addition

--- src/test/java/edu/brown/cs/student/kdtree/KDCalculatorTest.java
+++ src/test/java/edu/brown/cs/student/kdtree/KDCalculatorTest.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 
 public class KDCalculatorTest {
 
+  /**
+   * tests that the distance between two points is calculated correctly
+   * in the findDistance method
+   */
+
   @Test
   public void findDistanceTest() {
     KDCalculator kdcalc = new KDCalculator(null);
@@ -13,5 +18,7 @@ public class KDCalculatorTest {
     assertEquals(output, 1.732051, 0.001);
     output = kdcalc.findDistance(5, 3, 8, 1, 9, 3);
     assertEquals(output, 8.774964, 0.001);
+    output = kdcalc.findDistance(0,0,0,0,0,0);
+    assertEquals(output, 0, 0);
   }
 }
