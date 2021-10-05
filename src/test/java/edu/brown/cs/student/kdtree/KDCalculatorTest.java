3d44c6a83fadbc9f3cf38d08b34b163c5e07a821
dtsryBKJnugY3v2v <90650613+dtsryBKJnugY3v2v@users.noreply.github.com>
2021-10-04 22:16:01 -0400

    fixed KDcalculator test constructor

--- src/test/java/edu/brown/cs/student/kdtree/KDCalculatorTest.java
+++ src/test/java/edu/brown/cs/student/kdtree/KDCalculatorTest.java
@@ -11,29 +11,29 @@ public class KDCalculatorTest {
    * in the findDistance method
    */
 
-//  @Test
-//  public void findDistanceTest() {
-//    KDCalculator KDCalc = new KDCalculator(null);
-//    // simple tests with simple numbers
-//    double output = KDCalc.findDistance(1,1,0,0);
-//    assertEquals(output, 1.414214, 0.01);
-//
-//    output = KDCalc.findDistance(5, 3, 8, 1);
-//    assertEquals(output, 3.605551, 0.01);
-//
-//    // tests with zero distance
-//    output = KDCalc.findDistance(0,0,0,0);
-//    assertEquals(output, 0, 0.01);
-//
-//    output = KDCalc.findDistance(32,75,32,75);
-//    assertEquals(output, 0, 0.01);
-//
-//    // test with big numbers
-//    output = KDCalc.findDistance(3534573, 35479, 67896578, 567824);
-//    assertEquals(output, 64364206.503452, 0.01);
-//
-//    // test with doubles
-//    output = KDCalc.findDistance(45.155, 235.235, 75.35, 856.23);
-//    assertEquals(output, 621.728661, 0.01);
-//  }
+  @Test
+  public void findDistanceTest() {
+    KDCalculator KDCalc = new KDCalculator();
+    // simple tests with simple numbers
+    double output = KDCalc.findDistance(1,1,0,0);
+    assertEquals(output, 1.414214, 0.01);
+
+    output = KDCalc.findDistance(5, 3, 8, 1);
+    assertEquals(output, 3.605551, 0.01);
+
+    // tests with zero distance
+    output = KDCalc.findDistance(0,0,0,0);
+    assertEquals(output, 0, 0.01);
+
+    output = KDCalc.findDistance(32,75,32,75);
+    assertEquals(output, 0, 0.01);
+
+    // test with big numbers
+    output = KDCalc.findDistance(3534573, 35479, 67896578, 567824);
+    assertEquals(output, 64364206.503452, 0.01);
+
+    // test with doubles
+    output = KDCalc.findDistance(45.155, 235.235, 75.35, 856.23);
+    assertEquals(output, 621.728661, 0.01);
+  }
 }
