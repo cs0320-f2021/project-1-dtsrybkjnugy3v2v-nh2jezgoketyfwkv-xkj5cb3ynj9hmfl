cbb9d63204e6c24d94d12c64868f93a139b04373
dtsryBKJnugY3v2v <90650613+dtsryBKJnugY3v2v@users.noreply.github.com>
2021-10-12 18:09:34 -0400

    command interface documentation

--- src/main/java/edu/brown/cs/student/repl/Command.java
+++ src/main/java/edu/brown/cs/student/repl/Command.java
@@ -3,6 +3,11 @@ package edu.brown.cs.student.repl;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
+/**
+ * The command interface is implemented by all commands that you would like the REPL
+ * to recognize and run. All that the REPL needs to do with a command is know which String
+ * input is used to execute it and a means of telling it to run.
+ */
 public interface Command {
   String getCommand();
   void runCommand(String[] arguments)
